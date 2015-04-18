48d1fa52ddad2adcbe47a9ae510da442e73ae04a
Qingjun Chen <jun_lonely@126.com>
2015-04-18 20:41:44 +0800

    编程之美资格赛1-2

---
+++ ForJob/资格赛1.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Date
+{
+	int year, month, day;
+	void print(){ cout << year << "," << month << "-" << day << endl; }
+};
+
+int leap_count(int x)
+{
+	return x / 4 - x / 100 + x / 400;
+}
+
+int which_month(char *s)
+{
+	if (strcmp(s, "January") == 0) return 1;
+	if (strcmp(s, "February") == 0) return 2;
+	return 3;
+}
+
+void get_date(char *s, Date &d)
+{
+	char month[50];
+	sscanf(s, "%s %d, %d", month, &d.day, &d.year);
+	d.month = which_month(month);
+}
+
+int main_zige1()
+{
+	int T;
+	scanf("%d\n", &T);
+	for (int i = 1; i <= T; i++)
+	{
+		char s[100];
+		Date start, end;
+		gets(s); get_date(s, start);
+		gets(s); get_date(s, end);
+		int syear = start.month > 2 ? start.year + 1 : start.year;
+		int eyear = end.month < 2 || (end.month == 2 && end.day < 29) ? end.year - 1 : end.year;
+		int ans = leap_count(eyear) - leap_count(syear - 1);
+		printf("Case #%d: %d\n", i, ans);
+	}
+	return 0;
+}
---
+++ ForJob/资格赛2.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <unordered_map>
+
+using namespace std;
+
+int f[1001][1001];
+string s;
+unordered_map<char, int> last_map;
+int last[1001];
+const int modi = 100007;
+int main_zigesai2()
+{
+	int T;
+	cin >> T;
+	for (int tt = 1; tt <= T; tt++)
+	{
+		int ans = 0;
+		cin >> s;
+		int n = s.length();
+		last_map.clear();
+		memset(last, -1, sizeof(int) * n);
+		for (int i = 0; i < n; i++)
+		{
+			auto x = last_map.find(s[i]);
+			if (x != last_map.end())
+			{
+				last[i] = x->second;
+			}
+			last_map[s[i]] = i;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+				f[i][j] = 0;
+		}
+		for (int i = n - 1; i>=0; i--)
+		{
+			for (int j = i; j < n; j++)
+			{
+				f[i][j] += f[i + 1][j] + f[i][j - 1] - f[i + 1][j - 1];
+				if (s[i] == s[j])
+					f[i][j] += f[i + 1][j - 1] + 1;
+				f[i][j] %= modi;
+			}
+		}
+		printf("Case #%d: %d\n", tt, f[0][n - 1]);
+		/*
+		//on3
+		int ans = 0;
+		cin >> s;
+		int n = s.length();
+		last_map.clear();
+		memset(last, -1, sizeof(int) * n);
+		for (int i = 0; i < n; i++)
+		{
+		auto x = last_map.find(s[i]);
+		if (x != last_map.end())
+		{
+		last[i] = x->second;
+		}
+		last_map[s[i]] = i;
+		}
+		for (int i = 0; i < n; i++)
+		{
+		for (int j = 0; j < n; j++)
+		f[i][j] = 0;
+		f[i][i] = 1;
+		}
+
+		for (int i = 1; i < n; i++)
+		{
+			for (int j = i - 1; j >= 0; j--)
+			{
+				int k = last[i];
+				f[i][j] += f[i - 1][j] + 1;
+				while (k >= j)
+				{
+					f[i][j] += (f[i - 1][k + 1] + 1) % modi;
+					k = last[k];
+				}
+			}
+		}
+		
+		
+		printf("Case #%d: %d\n", tt, f[n - 1][0] % modi);
+		*/
+	}
+	return 0;
+}
